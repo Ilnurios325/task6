7eba1fd64d9d675c7a91e477f492d9104d03d16a
Ilnurios325 <ipilulka@gmail.com>
2023-01-07 19:36:37 +0300

    solution t6

---
+++ taskk6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("enter a number: ");
+int number = 
+Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("number "+number+" is EVEN");
+}
+else
+{
+    Console.WriteLine("number "+number+" is NOT EVEN");
+}           
